aecd9542fbda6a1ed7c59ec654e994980998d5e6
NHI122 <subham.behera@nslhub.com>
2022-05-10 15:39:45 +0530

    higher order function

--- src/MainClass.kt
+++ src/MainClass.kt
@@ -10,22 +10,32 @@ fun diceRolled(times:Int,range:IntRange,callBack:(result:Int)->Unit){
 
 //valued argument parameter
 
-fun diceRolled(callBack: ((result: Int) -> Unit)?=null):String{
+fun diceRolled(callBack: ((result: String) -> Unit)?=null){
+   callBack?.invoke("downloading Started")
     thread {
         Thread.sleep(3000)
-        callBack?.invoke(5)
+        callBack?.invoke(higherOrderMultiply(::multiply).toString())
     }
-    return "downloading Started"
+}
+
+fun add(a: Int, b: Int):Int=a + b
+
+
+fun higherFunc(addFunc:(Int,Int)-> Int){
+    val result = addFunc(3,6)
+    println("The sum of two numbers is: $result")
+}
+
+
+fun multiply(a:Int,b:Int):Int=a*b
+fun higherOrderMultiply(callBack: (Int,Int)->Int):Int{
+    return callBack(3,6)
 }
 
 
 fun main() {
-//    diceRolled(4,(1..6)){
-//        println(it)
-//    }
-    val value=diceRolled{
-        println(it)
-    }
-    println(value)
-
+//   diceRolled(4,(1..6)){ println(it) }
+    diceRolled{ println(it) }
+//    higherFunc(::add)
+//    higherOrderMultiply(::multiply)
 }

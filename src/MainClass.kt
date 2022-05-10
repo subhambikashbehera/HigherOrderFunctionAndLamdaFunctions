d7c646e6ee7b3fe49b46b4bb22d1535bb8be40ed
NHI122 <subham.behera@nslhub.com>
2022-05-10 14:21:16 +0530

    null argument

--- src/MainClass.kt
+++ src/MainClass.kt
@@ -1,6 +1,6 @@
+import kotlin.concurrent.thread
 
-
-
+//outPut is nothing
 fun diceRolled(times:Int,range:IntRange,callBack:(result:Int)->Unit){
     for (i in 0..times){
         val result=range.random()
@@ -8,14 +8,24 @@ fun diceRolled(times:Int,range:IntRange,callBack:(result:Int)->Unit){
     }
 }
 
+//valued argument parameter
 
+fun diceRolled(callBack: ((result: Int) -> Unit)?=null):String{
+    thread {
+        Thread.sleep(3000)
+        callBack?.invoke(5)
+    }
+    return "downloading Started"
+}
 
 
 fun main() {
-    diceRolled(4,(1..6)){
+//    diceRolled(4,(1..6)){
+//        println(it)
+//    }
+    val value=diceRolled{
         println(it)
     }
-
-
+    println(value)
 
 }
